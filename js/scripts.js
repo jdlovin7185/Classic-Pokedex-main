1e65aede5ff1f2e5b0159565a0da3a6ae43b5781
Josh Lovin <76003574+jdlovin7185@users.noreply.github.com>
2021-01-26 15:35:52 -0500

    Fixed array

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
-let pokemonList = ("Charizard", height = 1.7, type = ['fire','flying'])("Staryu", height = 0.8, type = ['water'])("Onix", height = 8.8, type = ['rock','ground'])("Starmie", height = 1.1, type = ['psychic','water'])
+const pokemonList = [
+  { name: "Charizard", height: 1.7, type: ['fire','flying'] },
+{name: "Staryu", height: 0.8, type: ['water'] },
+{name: "Onix", height: 8.8, type: ['rock','ground'] },
+{name: "Starmie", height: 1.1, type: ['psychic','water'] }
+]
