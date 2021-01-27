bfd3e7ec3a15423f914284171a98b79d30607cf5
Josh Lovin <76003574+jdlovin7185@users.noreply.github.com>
2021-01-27 14:30:22 -0500

    Adds height to pokemon w/description

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
-const pokemonList = [
+let pokemonList = [
   { name: "Charizard", height: 1.7, type: ['fire','flying'] },
 {name: "Staryu", height: 0.8, type: ['water'] },
 {name: "Onix", height: 8.8, type: ['rock','ground'] },
 {name: "Starmie", height: 1.1, type: ['psychic','water'] }
-]
+];
+
+for (let i=0; i < pokemonList.length; i++){
+  if (pokemonList[i].height <20 && pokemonList[i].height >1.6){
+    document.write(pokemonList[i].name + " height: " + pokemonList[i].height + " That's a big dude . . . ");
+  }else if (pokemonList[i].height <1) {
+    document.write(pokemonList[i].name + " height: " + pokemonList[i].height + " Dog friendly ");
+  }else {
+    document.write(pokemonList[i].name + " Kinda big but not really, probably would mess your day up ");
+  }
+}
