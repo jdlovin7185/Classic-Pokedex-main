1b14e1a5affa48ab11a3cb1b2ca93a969db5051b
Josh Lovin <76003574+jdlovin7185@users.noreply.github.com>
2021-02-01 13:00:35 -0500

    Cleaned up code

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,18 @@ let pokemonRepository = (function () {
   {name: "Staryu", height: 0.8, type: ['water'] },
   {name: "Onix", height: 8.8, type: ['rock','ground'] },
   {name: "Starmie", height: 1.1, type: ['psychic','water'] }
-]
+]   
+  function add(pokemon) {
+   pokemonList.push(pokemon);
+  }
+  
+  function getAll() {
+   return pokemonList;
+  }
+
   return {
-
-    add: function(pokemon) {
-      pokemonList.push(pokemon);
-    },
-
-    getAll:  function(){
-      return pokemonList;
-    }
+    add: add,
+    getAll:getAll
   };
 })();
 
